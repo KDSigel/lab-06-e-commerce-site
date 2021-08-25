6e5c11a0b97bc92945c6d18f047bca1563b79d7c
Karl Sigel <heykarl@gmail.com>
2021-08-25 15:25:58 -0700

    added new js file with getcart, setcart, and addtocart functions

---
+++ data/cart-api.js
@@ -0,0 +1,34 @@
+import { findById } from '../shoppingcart/utils.js';
+
+export function getCart() {
+    const stringCart = localStorage.getItem('CART');
+    if (!stringCart) {
+        return [];
+    }
+    const actualCart = JSON.parse(stringCart);
+    return actualCart;
+}
+
+export function setCart(cartArray) {
+    const stringCart = JSON.stringify(cartArray);
+    
+    localStorage.setItem('CART', stringCart);
+}
+
+export function addToCart(addedItemsId) {
+
+    const cart = getCart();
+    
+    const itsInCart = findById(cart, addedItemsId);
+
+    if (itsInCart) {
+        itsInCart.quantity++;
+    }
+    else {
+        const newCartItem = {
+            id: addedItemsId, quantity: 1
+        };
+        cart.push(newCartItem);
+    }
+    setCart(cart);
+}
